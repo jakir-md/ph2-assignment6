feada66520eb53053e9a0e49918d9c6b55ee122f
jakir-md <mdjakir.cse.ju@gmail.com>
2025-08-24 20:29:53 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button>This is a button</Button>
+    </>
+  )
+}
+
+export default App
